e063f547d5262476b817b6373e3b4eba9a0ed4be
Ryan Navarro <ryannavarro@ymail.com>
2021-06-05 13:11:50 -0700

    added formula and hex conversion

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 
+let customNum = document.getElementById('bandnum').value
+
+
+
+function selectNetwork(e){
+    document.getElementById("ltebandbtns").style.display = "none";
+    document.getElementById("nrbandbtns").style.display = "none";
+   if(e === "lte"){
+    console.log("test")
+        document.getElementById("ltebandbtns").style.display = "block";
+   }else if(e === "nr"){
+    document.getElementById("nrbandbtns").style.display = "block";
+   }else{
+       
+   }
+   
+}
 
 function band(num){
+  
   let y = (num-1);
-  let result = Math.pow(2,y);
+  let result = Math.pow(2,y).toString(16);
 
-  document.getElementById("result").innerHTML = "<h1>" + result + "</h1>";
+  document.getElementById("result").innerHTML = `<h1>0X${result}</h1>`;
   
 }
 
