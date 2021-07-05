eadaf17592835962c12aa97c663903b961decd07
Ryan Navarro <ryannavarro@ymail.com>
2021-07-05 15:20:16 -0700

    changed format of LTE buttons
    changed the format of the band buttons of LTE to match NR band buttons

--- main.js
+++ main.js
@@ -7,24 +7,25 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 //show and hide LTE and NR preset buttons
 function selectNetwork(e){
-
+  const clearBands = bands.length = 0;
     document.getElementById("ltebandbtns").style.display = "none";
     document.getElementById("nrbandbtns").style.display = "none";
 
    if(e === "lte"){
-   
+      
       document.getElementById("ltebandbtns").style.display = "block";
-
+      clearBands
    }else if(e === "nr"){
 
       document.getElementById("nrbandbtns").style.display = "block";
-
+      clearBands
    }
    
 }
 
 //hex toggle button true/false
 function toggleBtn(){
+
   if(hexToggle===true){
 
     hexToggle=false;
@@ -37,12 +38,16 @@ function toggleBtn(){
   
 }
 
+//band conversion function
+
 function band(bnd, id){
 
   const tagId = document.getElementById(id);
   const tempVal = bnd-1;
   let singleConvert = Math.pow(2,tempVal);
 
+
+      //add or remove band to array
       if(tagId.value==="true"){
 
         let positionOfRemoveBand = bands.indexOf(singleConvert);
@@ -54,16 +59,20 @@ function band(bnd, id){
       }else{
 
         tagId.value = "true";
-          bands.push(singleConvert);
+
+        bands.push(singleConvert);
           
       }
-  
+      //
       if(bands.length > 0){
+
         finalVal = bands.reduce(reducer);
         displayResult();
+
       }else{
-        console.log("empty");
+        
         displayResult();
+
       }
  
 }
@@ -72,16 +81,21 @@ function band(bnd, id){
 //display result in hex or decimal
 function displayResult(){
 
+const result =document.getElementById("result");
+
+
   if(hexToggle===true && bands.length > 0){
 
-    document.getElementById("result").innerHTML = `<h1 class="text-center">0X${finalVal.toString(16).toUpperCase()}</h1>`;
+    result.innerHTML = `<h1 class="text-center">0X${finalVal.toString(16).toUpperCase()}</h1>`;
 
   }else if(hexToggle===false && bands.length > 0){
 
-    document.getElementById("result").innerHTML = `<h1 class="text-center">${finalVal}</h1>`;
+    result.document.getElementById("result").innerHTML = `<h1 class="text-center">${finalVal}</h1>`;
 
-  } else{
-    document.getElementById("result").innerHTML = `<h1 class="text-center">Click bands</h1>`;
+  }else{
+
+    result.getElementById("result").innerHTML = `<h1 class="text-center">Click bands</h1>`;
+
   }
 
 }
